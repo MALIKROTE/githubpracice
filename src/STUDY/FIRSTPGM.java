aa3e00b41e96c0dcd42fc8ffe383c4ee2901c8d5
unknown <malikrote1@gmail.com>
2023-01-06 21:24:07 +0530

    framework

---
+++ src/STUDY/FIRSTPGM.java
@@ -0,0 +1,12 @@
+package STUDY;
+
+public class FIRSTPGM {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome Back Malik");
+
+	}
+
+}
